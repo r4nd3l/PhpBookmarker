da9d211788fdc345f70bd1331492f7cb385d8e7a
matemolnar88 <matemolnar88@gmail.com>
2019-01-16 21:36:24 +0100

    Fixes and MySql update

--- conn.php
+++ conn.php
@@ -1,9 +1,24 @@
 <?php
 
-//MySQLi 
-$conn = mysqli_connect("localhost","root","","sample");
+//MySQLi
+$conn = mysqli_connect("localhost","root","","bookmarker");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
+
+// sql to create table
+$sql = "CREATE TABLE bookmark (
+website_id INT(11) NOT NULL,
+website_name VARCHAR(30) NOT NULL,
+website_link VARCHAR(100) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "<h5 class='form-control shadow-sm text-center'><b>Database added successfully!</b></h5>";
+}
+
+//mysqli_close($conn);
+
 
 ?>
